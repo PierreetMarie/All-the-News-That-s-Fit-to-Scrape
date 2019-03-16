db6779145021c47a7071ba9190fe7845b83c0435
Enrique Sandino <44278564+PierreetMarie@users.noreply.github.com>
2019-03-16 09:21:02 -0400

    Add files via upload

--- ScrapingHomeWork/server.js
+++ ScrapingHomeWork/server.js
@@ -1,7 +1,46 @@
 
 var cheerio = require("cheerio");
-
+var express = require("express")
 var axios = require("axios");
+var app = express();
+var mongojs = require("mongojs");
+var database = "scraper";
+var collections = ["scrapedData"];
+
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(err) {
+  console.log("Database Error:", err);
+});
+
+app.get('/', function(req, res) {
+  res.send("Hello world");
+});
+
+app.get("/all", function(req, res) {
+  db.scrapedData.find({}, function(err, found) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+app.get('/scrape', function(req, res) {
+  request('https://news.ycombinator.com/', function(error, response, html) {
+    var $ = cheerio.load(html);
+      $('.title').each(function(i, element) {
+        var title = $(this).children('a').text();
+        var link = $(this).children('a').attr('href');
+          if (title && link) {
+            db.scrapedData.save({
+              title: title,
+            });
+          }
+      })
+    }
+  })
+})
 
 
 console.log("\n***********************************\n" +
@@ -9,11 +48,10 @@ console.log("\n***********************************\n" +
   "from reddit's webdev board:" +
   "\n***********************************\n");
 
+request("https://old.reddit.com/r/webdev/", function (error, response, html) {
 
-axios.get("https://old.reddit.com/r/webdev/").then(function (response) {
 
-
-  var $ = cheerio.load(response.data);
+  var $ = cheerio.load(html);
 
   var results = [];
 
@@ -21,7 +59,7 @@ axios.get("https://old.reddit.com/r/webdev/").then(function (response) {
   $("p.title").each(function (i, element) {
 
 
-    var title = $(element).text();
+    var title = $(this).text();
 
 
     var link = $(element).children().attr("href");
@@ -33,6 +71,6 @@ axios.get("https://old.reddit.com/r/webdev/").then(function (response) {
     });
   });
 
-  /
+  
   console.log(results);
 });
